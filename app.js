d3536e1e3dc6ba38b42901f97d4473a95e7f3e1b
Mudasirali79 <160850781+Mudasirali79@users.noreply.github.com>
2024-03-07 17:58:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// let capital = "VNSMJHDKKBDKDBKBDBKVDBD";
+// let small = "bshsbxdmncmjdkdbdabg";
+// let num = "2524445545671";
+// let character = "#$%%&^&*(";
+
+// let randomcapital = "";
+// let randomsmall = "";
+// let randomnum = "";
+// let randomcharacter = "";
+
+// for(let i = 0; i < 4; i++){
+//     let random = Math.floor( Math.random() * capital.length )
+//     randomcapital += capital[random] 
+
+//     let random1 = Math.floor( Math.random() * small.length )
+//     randomsmall += small[random1] 
+
+//     let random2 = Math.floor( Math.random() * num.length )
+//     randomnum += num[random2] 
+
+//     let random3 = Math.floor( Math.random() * character.length )
+//     randomcharacter += character[random3] 
+// }
+// let password = (randomcapital + randomsmall + randomnum + randomcharacter)
+// console.log(password)
+
+//------------------------------------------------------------------------------
+
+
+
+// let player1 = prompt("Enter First player Name")
+// let player2 = prompt("Enter Second player Name")
+
+// let random = Math.round ( Math.random() * 2 )
+
+// if ( random == 1 ){
+//     document.write( "Congratulations! " + player1 + " you won the toss" )
+// }
+// else{
+//     document.write( "Congratulations! " + player2 + " you won the toss" )
+// }
+
+//-----------------------------------------------------------------------------
+
